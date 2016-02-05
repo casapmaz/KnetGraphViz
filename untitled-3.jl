a5a9cc243417ea781c0c796b5bce2bef3d97f978
casapmaz <csapmaz14@ku.edu.tr>
2016-02-06 01:59:09 +0200

    i tried

---
+++ untitled-3.jl
@@ -0,0 +1,14 @@
+type crssn
+  id::Int
+  op::String
+  name::String
+  argv::Array{Int,1}
+end
+
+a= crssn(1, "input", "x", [0])
+b=crssn(2, "input2", "y", [0])
+d=crssn(7, "add", "add???", [1,2])
+L= [a, b, d]
+for i in 1:length(L)
+  println("{\nid: ", L[i].id, "\nop: ", L[i].op, "\nname: ", L[i].name, "\nargv: ", L[i].argv, "\n}")
+end
