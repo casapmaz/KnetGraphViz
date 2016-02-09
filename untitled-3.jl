e3108c862423bd5d437c03a077573b69f0daa666
casapmaz <csapmaz14@ku.edu.tr>
2016-02-09 21:10:33 +0200

    graphs fixed

--- untitled-3.jl
+++ untitled-3.jl
@@ -1,7 +1,8 @@
 using Graphs
 import Base.*
 import Base.print
-
+import Graphs.add_vertex!
+Pkg.update()
 
 type crssn
   id::Int
@@ -14,13 +15,13 @@ function print(p::crssn)
   println("{\nid: ", p.id, "\nop: ", p.op, "\nname: ", p.name, "\nargv: ", p.argv, "\n}")
 end
 
-*(k::crssn, l::crssn)=crssn(10, "matmul", "multiply", [k.id, l.id])
+*(k::crssn, l::crssn)=crssn(3, "matmul", "multiply", [k.id, l.id])
 
 
 #crssn instances
 
-a= crssn(1, "input", "x", [0])
-b=crssn(2, "input2", "y", [0])
+a= crssn(1, "input", "x", [])
+b=crssn(2, "input2", "y", [])
 d=crssn(3, "matmul", "multiply?", [1,2])
 L= [a, b, d]
 
@@ -37,26 +38,16 @@ print(h)
 
 
 #graphs
-g=graph(L, [])
 
-e1=make_edge(g, a, d)
-e2=make_edge(g, b, d)
-g=graph(L, [e1, e2])
-#i can't use add_edge! or make_edge for some reason
-#it says there's no method matching what i wrote
+tGraph=graph(crssn[], Edge{crssn}[])
+
+add_vertex!(tGraph, a)
+add_vertex!(tGraph, b)
+add_vertex!(tGraph, d)
+
+add_edge!(tGraph, a, d)
+add_edge!(tGraph, b, d)
 
 
-#i don't fully get how graphs work yet
-
-#testGraph=graph([1, 2, 4, 7], [])     ->makes a graph wit those vertices and zero edges
-#print(vertices(testGraph))            ->[1,2,4,7]
-#vertex_index(4, testGraph)            ->3
-#make_edge(testGraph, 1, 4)            ->seems to works BUT only makes the edge/doesn't add it to edgelist
-#add_edge!(testGraph, 1, 7)            ->gives error
-#add_edge!(testGraph, vertices(testGraph)[1], vertices(testGraph)[3])       ->gives the same error
-#j=make_edge(testGraph, 1, 4)
-#add_edge!(testGraph, j)               ->gives the same error
-#add_vertex!(testGraph, 5)             ->works fine
-#make_vertex(testGraph, 6)             ->gives the same error
 
 
